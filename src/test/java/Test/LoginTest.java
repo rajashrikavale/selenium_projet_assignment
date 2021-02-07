1f4067d139a5702b58ef94b4a15f95feb1a7c51f
rajashrikavale <77441293+rajashrikavale@users.noreply.github.com>
2021-02-07 14:32:52 +0530

    fix formatting

--- src/test/java/Test/LoginTest.java
+++ src/test/java/Test/LoginTest.java
@@ -1,32 +1,24 @@
 package Test;
 
+import java.io.IOException;
 
-	import java.io.IOException;
-
-	import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 import Pages.LoginPage;
 import io.qameta.allure.Description;
-	import io.qameta.allure.Severity;
-	import io.qameta.allure.SeverityLevel;
-	import io.qameta.allure.Story;
-	
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 
-	public class LoginTest extends BaseTest {
-		@Description("This is login test")
-		@Severity(SeverityLevel.BLOCKER)
-		@Story("OHM-01 - Login to the application")
-		@Test
-		public void login() throws IOException
-		{
-			LoginPage lp = new LoginPage(driver);
-			lp.enteringCredential();
-			
-		}
-		
-		
-		
+public class LoginTest extends BaseTest {
+	@Description("This is login test")
+	@Severity(SeverityLevel.BLOCKER)
+	@Story("OHM-01 - Login to the application")
+	@Test
+	public void login() throws IOException {
+		LoginPage lp = new LoginPage(driver);
+		lp.enteringCredential();
 
 	}
 
-
+}
